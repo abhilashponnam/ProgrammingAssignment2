a8233bd4e688925b8e19ce1bfebf967b807a478e
Ponnam Abhilash <abhilashponnam@gmail.com>
2016-04-18 14:50:22 +0530

    modified code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hi, makeCacheMatrix function, takes in matrix as argument and returns
+## both the input argument and inverse of that matrix as a list
+## //It also assigns the input argument to global variable getmatrix//
 
-## Write a short comment describing this function
+## casheSolve takes martrix as argument, compares it with input in makeCasheMatrix
+## If it is same as makeCasheMatrix input, it returns inverse of that matrix which is cached 
+## else it calculates the inverse and returns it
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix = function(x=matrix()) {
+  getmatrix <<- x
+  matrixinv <<- solve(x)
+  return(list(getmatrix,matrixinv))
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve = function(y = matrix()) {
+  z= identical(y,getmatrix)
+  if (z== TRUE)
+  { message ("getting cached data")
+    return(matrixinv) }
+  else
+  { return(solve(y))}
+  
 }
